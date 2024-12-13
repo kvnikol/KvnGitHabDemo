9e83eb2eb202cf578b94f696bb5b09bb2662c8b3
vladimir <vnklochaniy@gmail.com>
2024-12-13 15:01:52 +0200

    тестовый проэкт

--- app/src/main/java/com/kvladimir/kvngithabdemo/MainActivity.kt
+++ app/src/main/java/com/kvladimir/kvngithabdemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             KvnGitHabDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Владимир",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
